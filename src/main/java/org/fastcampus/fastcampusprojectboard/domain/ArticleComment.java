c03bf8c224e34e4dcd86dd751b8066646cf4184c
keaunsol Na <knsol1992@naver.com>
2024-04-01 19:33:00 +0900

    #8- 도메인 클래스 설계(게시글, 댓글)
    도메인 정보를 설계
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB와 연동을 위한 추가적인 표현은 추후 업데이트 될 수 있음.

---
+++ src/main/java/org/fastcampus/fastcampusprojectboard/domain/ArticleComment.java
@@ -0,0 +1,15 @@
+package org.fastcampus.fastcampusprojectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+
+    private long id;
+    private String article_id;          // 게시글 (ID)
+    private String content;             // 본문
+
+    private LocalDateTime createdAt;    // 생성일시
+    private String createdBy;           // 생성자
+    private LocalDateTime modifiedAt;   // 수정일시
+    private String modifiedBy;          // 수정자
+}
